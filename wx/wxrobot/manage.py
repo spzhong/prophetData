e1b27a8d441647254bf6e2ec111604ec9cd1713e
宋培众 <spzhongwin@126.com>
2019-04-18 19:27:57 +0800

    commit

--- wx/wxrobot/manage.py
+++ wx/wxrobot/manage.py
@@ -6,7 +6,7 @@ from werobot.replies import ArticlesReply, Article
 robot = WeRoBot(token='8cf561d3b980e1143ee962256805be24')
 # 明文模式不需要下面三项
 # robot.config["APP_ID"]='wxbb2b262238938195'
-# #robot.config["APP_SECRET"]=''
+# #robot.config["APP_SECRET"]='e5007656796347a71fbc7125ac1d17de'
 # robot.config['ENCODING_AES_KEY'] = 'OWWxHPqt4tkz274VtiLNcpGmQmerlk4E0lWazNUjMHP'
 
 
@@ -14,6 +14,15 @@ robot = WeRoBot(token='8cf561d3b980e1143ee962256805be24')
 @robot.subscribe
 def subscribe(message):
     return '你好，欢迎关注鸭先知数据！'
+
+
+# 取消关注
+@robot.unsubscribe
+def subscribe(message):
+     pass
+
+
+
 
 
 # 卡片 回复卡片
@@ -35,6 +44,35 @@ def blog(message):
 def echo(message):
     return message.content
 
+# @robot.image 修饰的 Handler 只处理图片消息
+@robot.image
+def img(message):
+    return message.img
+
 robot.config['HOST'] = '127.0.0.1'
 robot.config['PORT'] = 9001
 robot.run()
+
+
+
+# 获取操作的客户端
+
+# #自定义菜单
+# from werobot import WeRoBot
+# robot = WeRoBot()
+# robot.config["APP_ID"] = "你的 AppID"
+# robot.config["APP_SECRET"] = "你的 AppSecret"
+#
+# client = robot.client
+#
+# client.create_menu({
+#     "button":[{
+#          "type": "click",
+#          "name": "今日歌曲",
+#          "key": "music"
+#     }]
+# })
+#
+# @robot.key_click("music")
+# def music(message):
+#     return '你点击了“今日歌曲”按钮'
